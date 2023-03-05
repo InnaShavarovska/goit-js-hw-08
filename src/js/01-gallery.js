f25b4dff150eab3194e0558fbc7ca06dea612e9c
InnaShavarovska <innashavarovska@gmail.com>
2023-03-05 14:53:20 +0200

    +завдання 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,32 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+function creatCardImagesMarkup (images) {
+	return images.map(({ preview, original, description }) => {
+	  return `<li>
+	 <a class="gallery__item" href="${original}">
+	  <img
+		 class="gallery__image"
+		  src="${preview}"
+			alt="${description}"
+	  />
+	 </a>
+	</li>`
+	 }).join('');
+	};
+  
+  const galleryBox = document.querySelector('.gallery');
+  const cardsMarkup = creatCardImagesMarkup(galleryItems);
+  
+  galleryBox.insertAdjacentHTML('beforeend', cardsMarkup);
+  
+  
+  const lightbox = new SimpleLightbox('.gallery a', {
+	  captionsData: 'alt',
+	  captionDelay: 250,
+	});
+
+console.log(galleryItems);
